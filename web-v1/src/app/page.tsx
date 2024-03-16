51a50dccc2eb5c4823e2816d94edbafdb72d87dc
kris aziabor <krisaaziabor@gmail.com>
2024-03-16 15:54:04 -0400

    edits to v1

--- web-v1/src/app/page.tsx
+++ web-v1/src/app/page.tsx
@@ -8,11 +8,12 @@ function Background() {
       alt = "crush-background"
       placeholder = "blur"
       blurDataURL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII"
-      fill = {true}
       objectFit = "cover"
       quality = {100}
       sizes = "100vw"
       priority = {true}
+      width = {1920}
+      height = {1080}
     />
   )
 }
