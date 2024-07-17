d32665e4f79a24fed38abb4125143ab1017d1998
kris aziabor <krisaaziabor@gmail.com>
2024-07-16 21:45:32 -0400

    changed copy

--- web-v0/src/app/components/radix/collapsible.tsx
+++ web-v0/src/app/components/radix/collapsible.tsx
@@ -8,7 +8,7 @@ function Bio() {
 
   return (
     <h2>
-      Kris
+      I,
       <span> </span>
       <Collapsible.Root open={openAtteh} onOpenChange={setOpenAtteh} style={{display: 'inline'}}>
         <Collapsible.Trigger asChild>
@@ -28,16 +28,20 @@ function Bio() {
         </Collapsible.Content>
       </Collapsible.Root>
       <span> </span>
-      Aziabor is a software engineer, designer, and photographer.
+      am a software engineer, designer, and photographer.
       <br></br>
       <br></br>
-      Passionate to explore the intentional creations and destructions of institutional memory, Aziabor plays with minimalistic aesthetics when forming thought-provoking visuals. 
+      I am entering my third year as a Computing in the Arts major at Yale University and currently intern at Fidelity Investments as a Full-Stack Software Engineer. 
       <br></br>
       <br></br>
-      He currently attends Yale University and is a Full-Stack Software Engineering Intern at Fidelity Investments. 
+      Whether I am producing designs, photo series, or pieces of writing, the traditions of time and memory and their creation and destruction always seep heavily into my work.
       <br></br>
       <br></br>
-    While this website is <div style={{ textDecoration: 'underline', display: 'inline'}}>under renovation</div>, you can access his <em><a href="https://www.linkedin.com/in/krisaziabor/">Linkedin</a></em>, <em><a href="https://read.cv/krisaziabor">CV</a></em>, and <em><a href="https://github.com/krisaziabor/">Github</a></em>.
+      Sharing my love for combining minimalistic aesthetics and  thought-provoking messaging, I lead projects and contribute to organizations across the Yale campus such as Yale Visual Artists, Maison, Yale Pop-Up, and The New Journal.
+      <br></br>
+      <br></br>
+      As is the case for all my other work, this website is perpetually in progress.
+      While this website is <div style={{ textDecoration: 'underline', display: 'inline'}}>under renovation</div>, you can access his <em><a href="https://www.linkedin.com/in/krisaziabor/">Linkedin</a></em>, <em><a href="https://read.cv/krisaziabor">CV</a></em>, and <em><a href="https://github.com/krisaziabor/">Github</a></em>.
     </h2>
   );
 }
